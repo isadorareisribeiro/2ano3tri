5af06e9080f58c19b4d2e5acff0c5eda3932eb3d
isadorareisribeiro <163017456+isadorareisribeiro@users.noreply.github.com>
2024-09-23 11:02:32 -0300

    aula skjhdig

--- perguntas.js
+++ perguntas.js
@@ -1,7 +1,7 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'HISTÓRIA',
+    'Qual é o nome do maior Faraó do Egito?',
+    'Tutancâmon'
 )
 
 criaCartao(
@@ -11,9 +11,9 @@ criaCartao(
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'LITERATURA',
+    'Qual é a o bra mais conhecida de Machado de Assis ?',
+    'Memórias Póstumas de Brás Cubas'
 )
 
 criaCartao(
